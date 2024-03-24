d56d97f748500601af689216b75b44ae117a5b10
SamiUllah Shakil <samiullahshakil131@gmail.com>
2024-03-24 19:28:31 +0500

    upgrade in number guessing

--- index.js
+++ index.js
@@ -1,25 +1,99 @@
 #!/usr/bin/env node
 import inquirer from "inquirer";
-const randomNumber = 43;
-console.log("Hint:");
-console.log("1: Number is greater than 35 and less than 50");
-console.log("2: Number is a Prime Number");
-const { name, guessedNumber } = await inquirer.prompt([
+import ora from "ora";
+import chalk from "chalk";
+import fs from "fs";
+let continuePlaying = true;
+// Welcome message
+console.log(chalk.yellow.bold("Welcome to the Guessing Game!"));
+// Get player's name and gender
+const { name, gender } = await inquirer.prompt([
     {
         name: "name",
         message: "Your Name: "
     },
     {
         type: "list",
-        message: "Guess the number:",
-        name: "guessedNumber",
-        choices: ["39", "40", "43", "48"]
+        message: "Select your gender:",
+        name: "gender",
+        choices: ["boy", "girl"]
     }
 ]);
-console.log("Hello, " + name);
-if (parseInt(guessedNumber) === randomNumber) {
-    console.log("Congratulations !!!, You have guessed the right number :) ");
+// Main game loop
+while (continuePlaying) {
+    // Display spinner while generating random number
+    const spinner = ora("Generating random number...").start();
+    const randomNumber = generateRandomNumber();
+    spinner.stop();
+    // Display hints
+    console.log(chalk.green("Hint:"));
+    console.log(chalk.yellow("1: Number is greater than 35 and less than 50"));
+    console.log(chalk.yellow("2: Number is a Prime Number"));
+    const correctNumber = randomNumber.toString();
+    // Generate random options with only one correct option between 35 and 50
+    const choices = [];
+    while (choices.length < 4) {
+        const randomChoice = Math.floor(Math.random() * (50 - 35 + 1)) + 35;
+        if (!choices.includes(randomChoice.toString())) {
+            choices.push(randomChoice.toString());
+        }
+    }
+    const correctIndex = Math.floor(Math.random() * 4);
+    choices[correctIndex] = correctNumber;
+    // Player makes a guess
+    const { guessedNumber } = await inquirer.prompt([
+        {
+            type: "list",
+            message: "Guess the number:",
+            name: "guessedNumber",
+            choices: choices.sort(() => Math.random() - 0.5) // Shuffle options
+        }
+    ]);
+    // Display player's name
+    console.log("Hello, " + chalk.blue(name));
+    // Check if the guess is correct
+    if (guessedNumber === correctNumber) {
+        // Display congratulations message and animation
+        if (gender === "boy") {
+            console.log(chalk.blue.bold("Congratulations " + name + "!!!"));
+            console.log(chalk.blue(getAnimation("boy.txt")));
+        }
+        else {
+            console.log(chalk.magenta.bold("Congratulations " + name + "!!!"));
+            console.log(chalk.magenta(getAnimation("girl.txt")));
+        }
+        console.log(chalk.green("Congratulations !!!, You have guessed the right number :) "));
+        console.log(chalk.yellow(getAnimation("firework.txt")));
+    }
+    else {
+        // Display failure message and animation
+        console.log(chalk.red(getAnimation("heartbroken.txt")));
+        console.log(chalk.red("You lose!!! The correct number was " + correctNumber));
+    }
+    // Ask if the player wants to play again
+    const { playAgain } = await inquirer.prompt([
+        {
+            type: "confirm",
+            message: "Do you want to play again?",
+            name: "playAgain"
+        }
+    ]);
+    // If player chooses not to play again, exit the game loop
+    if (!playAgain) {
+        continuePlaying = false;
+        console.log(chalk.cyan("Thank you for playing!," + name));
+    }
 }
-else {
-    console.log("You lose!!! The correct number was " + randomNumber);
+// Function to generate a random number between 36 and 49 (inclusive)
+function generateRandomNumber() {
+    return Math.floor(Math.random() * (49 - 36 + 1)) + 36;
+}
+// Function to get animation from file
+function getAnimation(filename) {
+    try {
+        return fs.readFileSync(filename, "utf-8");
+    }
+    catch (error) {
+        return "";
+    }
 }
